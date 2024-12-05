aa36cc557f832ac64d7a955c0cdbd27da696d5a5
DTraitor <sehhd32@gmail.com>
2024-12-05 22:17:35 +0200

    Platforms and hook

--- scripts/hook.gd
+++ scripts/hook.gd
@@ -1,7 +1,10 @@
 class_name Hook
 extends CharacterBody2D
 
+signal hit_platform(platform: Node2D)
 
+
+var terminate_position_y: float
 var throw: bool = false
 
 
@@ -12,8 +15,14 @@ func _physics_process(delta: float) -> void:
 	velocity += get_gravity() * delta
 	var collision = move_and_collide(velocity * delta, false, true, true)
 	if collision:
+		hit_platform.emit(collision.get_collider())
 		queue_free()
 		return
+
+	if terminate_position_y < global_position.y:
+		queue_free()
+		return
+
 	rotation = velocity.angle()
 
 
